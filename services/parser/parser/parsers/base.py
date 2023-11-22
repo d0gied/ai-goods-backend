63f4a5286aaf580fa7f567521f23eef6910d209f
d0gied <german.n.u@mail.ru>
2023-11-22 18:14:05 +0300

    added tests

--- services/parser/parser/parsers/base.py
+++ services/parser/parser/parsers/base.py
@@ -1,32 +1,34 @@
 from abc import ABC
+
 from ..models import Good
+
 
 class Parser(ABC):
     def __init__(self, *args, **kwargs):
         pass
-    
+
     def get_goods(self, request: str, *, limit: int = 100, **kwargs) -> list[Good]:
-        """ Get goods from the site
-        
+        """Get goods from the site
+
         P.s. Without images
-        
+
         Args:
             request (str): request for search
-            limit (int, optional): Max amount of goods to search. Defaults to 100. 
+            limit (int, optional): Max amount of goods to search. Defaults to 100.
         Returns:
             list[Good]: list of goods
         """
-        
+
         raise NotImplementedError
 
     def get_good(self, good_id: str, **kwargs) -> Good:
-        """ Get good from the site
+        """Get good from the site
 
         Args:
             good_id (str): good id
-            
+
         Returns:
             Good: good
         """
-        
+
         raise NotImplementedError
--- services/parser/parser/tasks/base.py
+++ services/parser/parser/tasks/base.py
@@ -1,13 +1,11 @@
 import celery
 
-from ..enums import Sources
-
 
 class BaseTask(celery.Task):
     def __init__(self, name: str):
         super().__init__()
-        self.name = f'parser.tasks.{name}' 
-    
+        self.name = f"parser.tasks.{name}"
+
     def run(self, *args, **kwargs):
         """Run the task"""
-        raise NotImplementedError('Subclasses must implement this method')
+        raise NotImplementedError("Subclasses must implement this method")
