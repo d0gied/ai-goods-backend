3ed8c1b21f031fea98605501771016397f8bb749
d0gied <german.n.u@mail.ru>
2023-12-24 12:20:12 +0300

    updated

--- services/api/api/routers/__init__.py
+++ services/api/api/routers/__init__.py
@@ -1,11 +1,18 @@
+from fastapi import APIRouter
+
 from .embed import router as embed_router
 from .parse import router as parse_router
 from .search import router as search_router
 
+router = APIRouter(
+    prefix="/api",
+    tags=["api"],
+)
+
+router.include_router(embed_router)
+router.include_router(parse_router)
+router.include_router(search_router)
+
 
 def get_routers():
-    return [
-        embed_router,
-        parse_router,
-        search_router,
-    ]
+    return router
